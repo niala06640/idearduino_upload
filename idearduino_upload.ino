9cc08db0a348cc4b3e51493ebb831ee7aac2d8a8
06GitHub <06userit@gmail.com>
2024-05-18 10:00:07 +0200

    Première sauvegarde test upload Arduino IDE

---
+++ idearduino_upload.ino
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  delay(2000);
+  ESP_LOGI("","setup() done");
+}
+
+void loop() {
+  delay(2000);
+  ESP_LOGI("","loop");
+  Serial.print("loop !");
+}
